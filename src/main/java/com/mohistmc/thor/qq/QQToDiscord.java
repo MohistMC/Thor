42ef9a09c1fd47de457eb7510eafe6f1fb94c182
Shawiiz_z <55449195+Shawiizz@users.noreply.github.com>
2021-07-02 17:58:25 +0200

    Ignore case & add space to translated messages

--- src/main/java/com/mohistmc/thor/qq/QQToDiscord.java
+++ src/main/java/com/mohistmc/thor/qq/QQToDiscord.java
@@ -71,9 +71,9 @@ public class QQToDiscord extends SimpleListenerHost {
 		if(!e.getMessage().contentToString().equals("[图片]") && !e.getMessage().contentToString().equals("[表情]")) {
 			try {
 				String translation = translate(originalMsg, "en");
-				if(translation.length() == 0 || translation.equals(msg)) throw new Exception();
+				if(translation.length() == 0 || translation.equalsIgnoreCase(msg)) throw new Exception();
 
-				builder.setContent(msg + "\n--------\n" + translation);
+				builder.setContent(msg + "\n--\n" + translation+"\n** **");
 			} catch (Exception ex) { //Failed to translate or translation isn't needed, just send the original message
 				builder.setContent(msg);
 			}
